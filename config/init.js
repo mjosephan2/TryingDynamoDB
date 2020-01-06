1fcecb6601c14360dfc63553f3d9716af8d732ac
mjosephan2 <mario.kosasih@gmail.com>
2020-01-07 03:48:34 +0800

    init

--- config/init.js
+++ config/init.js
@@ -3,8 +3,8 @@ const AWS = require("aws-sdk");
 const config = {
     region: "ap-southeast-1",
     credentials:{
-        accessKeyId: "AKIA4756MKT7H5ELKMOE",
-        secretAccessKey: "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"
+        accessKeyId: "PRIVATE",
+        secretAccessKey: "PRIVATE"
     }
 }
 
